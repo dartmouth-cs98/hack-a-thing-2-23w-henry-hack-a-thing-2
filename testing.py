2f407c80a2430785afb18437103d956b88b96343
henry <henry.b.mans.23@dartmouth.edu>
2023-01-30 13:26:27 -0500

    used nltk built-in frequency dist. also removed stop words and punctuation

--- testing.py
+++ testing.py
@@ -2,5 +2,9 @@ from tokenize_from_file import *
 
 filename = "sample_text/twins.txt"
 
-print(frequency_count(filename))
+freq_dist = frequency_dist(filename)
+
+print(freq_dist.most_common(20))
+
+
 
--- tokenize_from_file.py
+++ tokenize_from_file.py
@@ -1,5 +1,7 @@
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+from nltk import FreqDist
+from string import punctuation
 
 
 def tokenize_from_file(filename):
@@ -29,3 +31,21 @@ def frequency_count(filename):
             frequency_table[token] += 1
 
     return frequency_table
+
+def frequency_dist(filename):
+
+    file_tokens = tokenize_from_file(filename)
+
+    # remove stop words and tokens that are just punctuation
+    stop_words = set(stopwords.words("english"))
+
+    meaningful_tokens = []
+    for token in file_tokens:
+        if any(p in token for p in punctuation):
+            continue
+        if token not in stop_words:
+            meaningful_tokens.append(token)
+
+    frequency_dist = FreqDist(meaningful_tokens)
+
+    return frequency_dist
