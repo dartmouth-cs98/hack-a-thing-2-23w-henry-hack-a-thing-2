8de8203a86461aa558ad29dd8f163aab226f3481
henry <henry.b.mans.23@dartmouth.edu>
2023-01-31 13:06:29 -0500

    quiz works as before with the official google cloud api

--- generate_quiz.py
+++ generate_quiz.py
@@ -1,19 +1,18 @@
 from tokenize_from_file import *
 from googletrans import Translator
 from wordfreq import zipf_frequency
-from google.cloud import translate
+from google_api import translate_text
 
 def get_candidates(filename, source_lang = "en", target_lang = "es"):
     candidates = frequency_dist(filename)
 
     quiz_candidates = []
-    translator = Translator()
 
     for candidate in candidates.most_common(25):
         source_word = candidate[0].lower()
-        translation = translator.translate(source_word, dest=target_lang, src=source_lang)
+        translation = translate_text(source_word)[0].translated_text
 
-        target_word = translation.text.lower()
+        target_word = translation.lower()
 
         if source_word != target_word and zipf_frequency(source_word, 'en') < 6:
             quiz_candidates.append((source_word, target_word, candidate[1]))
